a3bcd7680a95b183da04f6606e84da06941a1a13
Kaoutar Hosseinat <kaoutarhosseinat@gmail.com>
2024-05-02 16:09:34 +0100

    tables created

--- SQL/projectTables.sql
+++ SQL/projectTables.sql
@@ -2,13 +2,52 @@ use AGENCE_IMMOBILIER
 /
 
 CREATE TABLE BIEN_IMMOBILIERS(
-    id_bienimm number(10) PRIMARY KEY auto_increment,
-    typebi varchar2(30),
-    taille number(10,10),
-    PRIX number(10,10),
-    localisation varchar2(50),
-    descbi varchar2(100),
-    ID_Agent number(10),
+    id_bienimm number(10) PRIMARY KEY auto_increment not null,
+    typebi varchar2(30) not null,
+    taille number(10,10) not null,
+    PRIX number(10,10) not null,
+    localisation varchar2(50) not null,
+    descbi varchar2(100) not null,
+    ID_Agent number(10) not null,
     FOREIGN KEY(ID_Agent)
-    REFERENCES agent_immb(ID_Agent),
+    REFERENCES agent_immb(ID_Agent)
 )
+/
+CREATE TABLE CLIENT(
+    id_client number(10) PRIMARY KEY auto_increment not null,
+    nomc varchar2(20) not null,
+    prenomc varchar2(20) not null,
+    typec varchar2(15) not null,
+    demandec varchar2(100) not null,
+    numTransaction number(10) auto_increment not null,
+    FOREIGN KEY(numTransaction)
+    REFERENCES transactions(numTransaction)
+)
+/
+CREATE TABLE transactions(
+    numTransaction number(10) PRIMARY KEY auto_increment not null,
+    typeT varchar2(15) not null,
+    paiement number(10) not null,
+    dateT date not null ,
+    id_client number(10) not null,
+    FOREIGN KEY(id_client)
+    REFERENCES CLIENT(id_client),
+    id_bienimm number(10) not null,
+    FOREIGN KEY (id_bienimm)
+    REFERENCES BIEN_IMMOBILIERS(id_bienimm)
+)
+/
+CREATE TABLE rendez_vous(
+    id_rdv number(10) not null ,
+    date_rdv date not null,
+    id_client number(10) not null,
+    FOREIGN KEY (id_client)
+    REFERENCES CLIENT (id_client)
+)
+/
+CREATE TABLE agent_immb(
+    ID_Agent number (10) not null,
+    noma varchar2(20) not null,
+    prenoma varchar2(20) not null
+)
+/
